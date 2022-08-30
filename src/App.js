66467679a2bcc755c9cd26868c2e5dfd53c8a807
Your Name <karkibishal00@gmail.com>
2022-08-30 10:10:01 +1000

    register form created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Dashboard } from "./pages/dashboard/Dashboard";
+
+import { AdminRegister } from "./pages/register/AdminRegister";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />{" "}
+          <Route path="/register" element={<AdminRegister />} />{" "}
+        </Routes>{" "}
+      </BrowserRouter>{" "}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { AdminLayout } from "../../components/layout/AdminLayout";
+
+export const Dashboard = () => {
+  return (
+    <AdminLayout>
+      <Container>
+        <h1>Dashboard</h1>
+      </Container>
+    </AdminLayout>
+  );
+};
