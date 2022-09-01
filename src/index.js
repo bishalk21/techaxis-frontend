031b87a319996c2ff7c632c609cfedbd36e3e204
Your Name <karkibishal00@gmail.com>
2022-09-01 23:24:08 +1000

    frontend ready

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import DataProvider from './redux/store'
+ReactDOM.render(
   <React.StrictMode>
+    <DataProvider>
     <App />
-  </React.StrictMode>
+    </DataProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
